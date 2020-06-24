86ae944716b94c2bd57578cce055b02038ba17ee
최웅 <ung6039@naver.com>
2020-06-24 23:53:53 +0900

    asasa

--- SimpleTableProject/src/main/java/com/home/table/BoardMapper.java
+++ SimpleTableProject/src/main/java/com/home/table/BoardMapper.java
@@ -3,9 +3,12 @@ package com.home.table;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 
 public interface BoardMapper {
 	
-	@Select("SELECT * FROM BOARD order by no")
+	
+	
+	@Select("SELECT no,title FROM BOARD order by no")
 	public List<BoardVO> BoardList();
 }
